6f29ede9b0ae28500b8a4260f419e9cd38079c31
sai kadimisetty <saikadimisetty@gmail.com>
2019-10-10 20:11:21 +0530

    initial commit

---
+++ src/main/java/com/axiata/test/AxiataTestApplication.java
@@ -0,0 +1,10 @@
+package com.axiata.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AxiataTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AxiataTestApplication.class, args);
+  }
+}
